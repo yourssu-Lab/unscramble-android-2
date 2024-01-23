40becf4ec58f764553ee0e887bec8c22b4a5376d
LEE YOU BIN <youbeene3409@naver.com>
2024-01-23 20:49:38 +0900

    chore : 파일 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,9 +36,15 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("com.github.yourssu:YDS-Android:2.5.2")
+    implementation("com.github.yourssu:YDS-Android:2.3.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+
+    implementation("com.google.dagger:hilt-android:2.48.1")
+
+
 }
