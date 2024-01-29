83856f4b7cafb163497bd5edca0cf7f962727f60
findwhatiwant <jws0041@naver.com>
2024-01-29 22:41:56 +0900

    feat : 텍스트필드 영문대소문자, 공백 체크

--- app/src/main/java/com/ursssu/unscramble/presentation/GameStartFragment.kt
+++ app/src/main/java/com/ursssu/unscramble/presentation/GameStartFragment.kt
@@ -1,17 +1,28 @@
 package com.ursssu.unscramble.presentation
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.fragment.findNavController
 import com.ursssu.unscramble.R
 import com.ursssu.unscramble.databinding.FragmentGameStartBinding
 import com.ursssu.unscramble.util.binding.BaseFragment
+import com.yourssu.design.system.atom.BoxButton
+import com.yourssu.design.system.atom.EditText
+import com.yourssu.design.system.foundation.Icon
+
 
 class GameStartFragment : BaseFragment<FragmentGameStartBinding>(R.layout.fragment_game_start) {
+    val submitType: MutableLiveData<Int> = MutableLiveData(BoxButton.TINTED)
+    val skipType: MutableLiveData<Int> = MutableLiveData(BoxButton.LINE)
+    val size: MutableLiveData<Int> = MutableLiveData(BoxButton.Large)
+    val rounding: MutableLiveData<Int> = MutableLiveData(8)
+    val isdisabled: MutableLiveData<Boolean> = MutableLiveData(false)
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initClickListener()
 
     }
@@ -20,11 +31,23 @@ class GameStartFragment : BaseFragment<FragmentGameStartBinding>(R.layout.fragme
         initGameStartClickListener()
     }
 
+    private fun checkText(text: String):Boolean{
+        if(text.length == 0) return false
+        for(i in 0..text.length-1){
+            if(!((text[i] in 'A'..'Z') || (text[i] in 'a'..'z'))){
+                return false
+            }
+        }
+        return true
+    }
 
     private fun initGameStartClickListener() {
         binding.btnGameStart.setOnClickListener {
-            findNavController().navigate(R.id.endFragment)
+            if(checkText(binding.textfiledGameStart.text.toString())) {
+                findNavController().navigate(R.id.endFragment)
+            }else{
+                //텍스트필드 헬퍼라벨텍스트 붉은 글씨 넣는 기능
+            }
         }
     }
-
 }
