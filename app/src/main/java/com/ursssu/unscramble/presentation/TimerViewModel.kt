582d209a2231edb3e3ecfbd43408f8eebd5c0855
LEE YOU BIN <youbeene3409@naver.com>
2024-02-05 12:06:09 +0900

    chore : 타이머 설정 UI

--- app/src/main/java/com/ursssu/unscramble/presentation/TimerViewModel.kt
+++ app/src/main/java/com/ursssu/unscramble/presentation/TimerViewModel.kt
@@ -6,10 +6,4 @@ import androidx.lifecycle.ViewModel
 class TimerViewModel : ViewModel() {
     val buttonText: MutableLiveData<String> = MutableLiveData("START")
 
-    val fieldLabelTextString = MutableLiveData<String>("fieldLabelText")
-
-    val isPositive: MutableLiveData<Boolean> = MutableLiveData(false)
-    val isNegative: MutableLiveData<Boolean> = MutableLiveData(false)
-    val isDisable: MutableLiveData<Boolean> = MutableLiveData(false)
-
 }
