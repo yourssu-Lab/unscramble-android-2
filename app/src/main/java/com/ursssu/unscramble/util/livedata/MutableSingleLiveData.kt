9fc24f0b7ca6741e36c7321bb22fca134d2e9402
giovannijunseokim <kingjunseo@gmail.com>
2024-02-06 02:32:38 +0900

    feat : add MutableSingleLiveData Class

---
+++ app/src/main/java/com/ursssu/unscramble/util/livedata/MutableSingleLiveData.kt
@@ -0,0 +1,16 @@
+package com.ursssu.unscramble.util.livedata
+
+class MutableSingleLiveData<T> : SingleLiveData<T> {
+
+    constructor() : super()
+
+    constructor(value: T) : super(value)
+
+    public override fun postValue(value: T) {
+        super.postValue(value)
+    }
+
+    public override fun setValue(value: T) {
+        super.setValue(value)
+    }
+}
