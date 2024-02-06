dbf198cc2d3fbe7ef2cba2fc306afaa03b934b2c
giovannijunseokim <kingjunseo@gmail.com>
2024-02-06 23:00:29 +0900

    feat : Event 클래스 상속 불가능 하도록 변경

--- app/src/main/java/com/ursssu/unscramble/util/livedata/Event.kt
+++ app/src/main/java/com/ursssu/unscramble/util/livedata/Event.kt
@@ -3,7 +3,7 @@ package com.ursssu.unscramble.util.livedata
 /**
  * Used as a wrapper for data that is exposed via a LiveData that represents an event.
  */
-open class Event<out T>(private val content: T) {
+class Event<out T>(private val content: T) {
 
     var hasBeenHandled = false
         private set // Allow external read but not write
