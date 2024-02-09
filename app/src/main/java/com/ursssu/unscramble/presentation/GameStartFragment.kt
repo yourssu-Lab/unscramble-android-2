e1d460d2d27b82d5afdab66af6abb132d8c271e1
Wes, 조우식 <50237635+findwhatiwant@users.noreply.github.com>
2024-02-09 21:02:59 +0900

    [feat#5] 게임 시작 UI 및 기능 일부 구현 (#16)
    * feat : Ui 초기 세팅
    
    * feat : 텍스트필드 영문대소문자, 공백 체크
    
    * chore : UI 글자 삽입 및 잘못된 문장 입력 시 헬퍼라벨텍스트
    
    * chore : 타이머 배경 타원 도형 추가
    
    * chore : 단어, 타이머 텍스트 UI 추가
    
    * chore : 소프트웨어 키보드에 따른 ui 리사이즈
    
    * chore : Submit 버튼 id 변경
    
    * chore : id 수정, 가독성 개선
    
    * chore : 가독성 수정
    
    * chore : 코드 자동 포맷팅

--- app/src/main/java/com/ursssu/unscramble/presentation/GameStartFragment.kt
+++ app/src/main/java/com/ursssu/unscramble/presentation/GameStartFragment.kt
@@ -6,24 +6,50 @@ import androidx.navigation.fragment.findNavController
 import com.ursssu.unscramble.R
 import com.ursssu.unscramble.databinding.FragmentGameStartBinding
 import com.ursssu.unscramble.util.binding.BaseFragment
+import com.yourssu.design.system.atom.BoxButton
+
 
 class GameStartFragment : BaseFragment<FragmentGameStartBinding>(R.layout.fragment_game_start) {
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initClickListener()
+
+        binding.textGameStartTimer.text = "5 : 31"
+
+        binding.btnGameStartSubmit.text = "Submit"
+
+        binding.btnGameStartSkip.text = "Skip"
+        binding.btnGameStartSkip.type = BoxButton.LINE
+
+        binding.textfieldGameStart.helperLabelText = ""
+
+        binding.textGameStartWord.text = "test"
 
     }
 
-    private fun initClickListener(){
+    private fun initClickListener() {
         initGameStartClickListener()
     }
 
+    private fun checkText(text: String): Boolean {
+        if (text.isEmpty()) return false
+        for (i in 0 until text.length) {
+            if (!((text[i] in 'A'..'Z') || (text[i] in 'a'..'z'))) {
+                return false
+            }
+        }
+        return true
+    }
 
     private fun initGameStartClickListener() {
-        binding.btnGameStart.setOnClickListener {
-            findNavController().navigate(R.id.endFragment)
+        binding.btnGameStartSubmit.setOnClickListener {
+            if (checkText(binding.textfieldGameStart.text.toString())) {
+                findNavController().navigate(R.id.endFragment)
+            } else {
+                binding.textfieldGameStart.helperLabelText = "영문 대 소문자만 사용가능합니다."
+            }
         }
     }
 
