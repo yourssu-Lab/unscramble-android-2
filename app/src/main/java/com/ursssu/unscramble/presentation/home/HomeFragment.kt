c9eae140a8379d6c4372777e3b775a16d8abc4b1
giovannijunseokim <kingjunseo@gmail.com>
2024-02-14 23:07:35 +0900

    chore : initDataBinding -> bindViewModel로 함수명 변경

--- app/src/main/java/com/ursssu/unscramble/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/ursssu/unscramble/presentation/home/HomeFragment.kt
@@ -14,11 +14,11 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initDataBinding()
+        bindViewModel()
         initObservers()
     }
 
-    private fun initDataBinding() {
+    private fun bindViewModel() {
         binding.viewModel = viewModel
     }
 
