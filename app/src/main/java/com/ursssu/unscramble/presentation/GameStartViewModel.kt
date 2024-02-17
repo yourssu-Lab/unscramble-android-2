d58df222e2e98cb89876f62d63d0ffef8de3fa53
findwhatiwant <jws0041@naver.com>
2024-02-18 01:54:10 +0900

    chore : 단어 검사 정규식으로 수정

--- app/src/main/java/com/ursssu/unscramble/presentation/GameStartViewModel.kt
+++ app/src/main/java/com/ursssu/unscramble/presentation/GameStartViewModel.kt
@@ -1,26 +1,21 @@
 package com.ursssu.unscramble.presentation
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class GameStartViewModel : ViewModel() {
 
-    val textFieldHelperLabelText : MutableStateFlow<String> = MutableStateFlow("")
-    val GameStartWordText : MutableStateFlow<String> = MutableStateFlow("test")
-    val GameStartTimerText : MutableStateFlow<String> = MutableStateFlow("5 : 31")
+    val textFieldHelperLabelText: MutableStateFlow<String> = MutableStateFlow("")
+    val GameStartWordText: MutableStateFlow<String> = MutableStateFlow("test")
+    val GameStartTimerText: MutableStateFlow<String> = MutableStateFlow("5 : 31")
 
     private fun checkText(text: String): Boolean {
+        val regex = Regex("[A-Za-z]+")
         if (text.isEmpty()) return false
-        for (i in 0 until text.length) {
-            if (!((text[i] in 'A'..'Z') || (text[i] in 'a'..'z'))) {
-                return false
-            }
-        }
-        return true
+        return regex.matches(text)
     }
 
-    fun onBtnGameStartSubmit(text: String){
+    fun onBtnGameStartSubmit(text: String) {
         if (checkText(text)) {
 
         } else {
